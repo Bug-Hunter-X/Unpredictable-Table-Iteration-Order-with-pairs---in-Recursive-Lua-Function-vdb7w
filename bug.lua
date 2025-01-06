c3878c0042d79b180a0bac248aca67cb6bced8a5
BugHunter-X <askzenai@gmail.com>
2025-01-06 01:08:04 +0100

    Add bug.lua file

---
+++ bug.lua
@@ -0,0 +1,11 @@
+local function foo(t)
+  for k, v in pairs(t) do
+    if type(v) == "table" then
+      foo(v)
+    end
+  end
+end
+
+local t = {a = 1, b = {c = 2, d = {e = 3}}}
+foo(t)
+print(t.b.d.e)
